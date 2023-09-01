9f992f03d65cf4599576c4465d8d84bfbb05e56f
Ulisses Malanski <malanskiwork@gamil.com>
2023-09-01 00:10:00 -0300

    feat: create vite project with global styled themes config

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  base: {
+    background: '#FAFAFA',
+    white: '#FFFFFF',
+    
+    'base-title': '#272221',
+    'base-subtitle': '#403937',
+    'base-text': '#574F4D',
+    'base-label': '#8D8686',
+    'base-hover': '#D7D5D5',
+    'base-button': '#E6E5E5',
+    'base-input': '#EDEDED',
+    'base-card': '#F3F2F2',
+  },
+  product: {
+    'purple-light': '#EBE5F9',
+    'purple-dark': '#4B2995',
+    'purple-': '#8047F8',
+  }
+}
